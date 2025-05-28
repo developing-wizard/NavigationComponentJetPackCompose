151f8ad06e1cd5f59a40e07600b54849194e8fdd
developing-wizard <talha.m00001@gmail.com>
2025-05-29 00:23:08 +0500

    Added Screenshots of App

--- app/src/main/java/com/example/firstdemoprojectcompose/navigation/screens/main/ProfileScreen.kt
+++ app/src/main/java/com/example/firstdemoprojectcompose/navigation/screens/main/ProfileScreen.kt
@@ -1,10 +1,31 @@
 package com.example.firstdemoprojectcompose.navigation.screens.main
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun ProfileScreen(innerPadding: PaddingValues){
-
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+            .background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Your Profile",
+            color = Color.White,
+            fontSize = 18.sp
+        )
+    }
 }
